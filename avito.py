bbfc70fc7509190f1f885788c0b04d2608ae08b3
Kapral ALeksandr <a.kapral26@gmail.com>
2018-09-19 21:49:46 +0300

    Begining test for parsing avito, return last page

--- avito.py
+++ avito.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3.5
+# ! /usr/bin/python3.5
 
 # План
 # 1. Сколько страниц
@@ -32,117 +32,128 @@ template_task = "https://www.avito.ru/krasnodarskiy_kray/mototsikly_i_mototehnik
 
 months = ['Января',	'Февраля',	'Марта',	'Апреля',	'Мая',	'Июня',	'Июля',	'Августа', 'Сентября',	'Октября',	'Ноября',	'Декабря']
 
-def get_html(url, useragent=None, proxy=None):
-	r = requests.get(url, headers=useragent, proxies=proxy)
-	return r.text
+class page_info:
 
-def get_last_page(html):
-	soup = BeautifulSoup(html, 'lxml')
-	last_page = soup.find('a', class_='pagination-page', text='Последняя' )
-	# print(last_page)
-	num_last_page = last_page.get('href')[-2:]
-	return int(num_last_page)+1
+	def __init__(self, link):
+		self.link = link
 
-def write_csv(data):
-	with open('main.csv', 'a', newline='') as f:
-		writer = csv.writer(f, delimiter=';')
-		writer.writerow((data['name'], data['price'], data['city'], data['time_publc'] ,data['link']))
+	def get_html(self, useragent=None, proxy=None):
+		r = requests.get(self.link, headers=useragent, proxies=proxy)
+		self.text_page =  r.text
 
-def normal_date(date):
-	day = date.strftime('%d')
-	month = months[ int(date.strftime('%m'))-1]
-	return day + ' ' + month
+	def get_last_page(self, html):
+		html = self.text_page
+		soup = BeautifulSoup(html, 'lxml')
+		last_page = soup.find('a', class_='pagination-page', text='Последняя' )
+		# print(last_page)
+		num_last_page = last_page.get('href')[-2:]
+		return int(num_last_page)+1
 
-def dell_all_probel(text):
-	return text.lstrip().rstrip()
+# def write_csv(data):
+# 	with open('main.csv', 'a', newline='') as f:
+# 		writer = csv.writer(f, delimiter=';')
+# 		writer.writerow((data['name'], data['price'], data['city'], data['time_publc'] ,data['link']))
 
-def get_page_date(html):
-	soup = BeautifulSoup(html, 'lxml')
-	# all_info = soup.find_all('div',class_='item_table-header')
-	all_info = soup.find_all('div',class_='description item_table-description snippet-experiment-wrapper')
-	# print('count_all_div', len(all_info))
-	main_info = {}
-	count = 0
-	for inf in all_info:
-		name = inf.find('a', class_='item-description-title-link').find('span').text.strip()
-		link = inf.find('a', class_='item-description-title-link').get('href')
-		price = inf.find('span', class_='price').get('content')
-		city = inf.find('div', class_='data').find('p').text.strip()
-		time_publc = inf.find('div', class_='js-item-date c-2').get('data-absolute-date')
-		today = datetime.now().date()
-		yesterday = today - timedelta(1)
-		if re.findall('Сегодня', time_publc):
-			time_publc = time_publc.replace('Сегодня', normal_date(today))
-		elif re.findall('Вчера', time_publc):
-			time_publc = time_publc.replace('Вчера', normal_date(yesterday))
+# def normal_date(date):
+# 	day = date.strftime('%d')
+# 	month = months[ int(date.strftime('%m'))-1]
+# 	return day + ' ' + month
 
-		if price == '0':
-			price = 'Цена не указана'
-		for serch in array_spec_words:
-			res = re.findall(serch.lower(), dell_all_probel(name.lower()))
-			if res != []:
-				main_info = {'name': dell_all_probel(name), 'price':dell_all_probel(price), 'city':dell_all_probel(city), 'time_publc':dell_all_probel(time_publc), 'link': dell_all_probel('https://www.avito.ru' + link)}
-				write_csv(main_info)
-				count +=1
-				print(count)
-			else:
-				continue
+# def dell_all_probel(text):
+# 	return text.lstrip().rstrip()
+
+# def get_page_date(html):
+# 	soup = BeautifulSoup(html, 'lxml')
+# 	# all_info = soup.find_all('div',class_='item_table-header')
+# 	all_info = soup.find_all('div',class_='description item_table-description snippet-experiment-wrapper')
+# 	# print('count_all_div', len(all_info))
+# 	main_info = {}
+# 	count = 0
+# 	for inf in all_info:
+# 		name = inf.find('a', class_='item-description-title-link').find('span').text.strip()
+# 		link = inf.find('a', class_='item-description-title-link').get('href')
+# 		price = inf.find('span', class_='price').get('content')
+# 		city = inf.find('div', class_='data').find('p').text.strip()
+# 		time_publc = inf.find('div', class_='js-item-date c-2').get('data-absolute-date')
+# 		today = datetime.now().date()
+# 		yesterday = today - timedelta(1)
+# 		if re.findall('Сегодня', time_publc):
+# 			time_publc = time_publc.replace('Сегодня', normal_date(today))
+# 		elif re.findall('Вчера', time_publc):
+# 			time_publc = time_publc.replace('Вчера', normal_date(yesterday))
+
+# 		if price == '0':
+# 			price = 'Цена не указана'
+# 		for serch in array_spec_words:
+# 			res = re.findall(serch.lower(), dell_all_probel(name.lower()))
+# 			if res != []:
+# 				main_info = {'name': dell_all_probel(name), 'price':dell_all_probel(price), 'city':dell_all_probel(city), 'time_publc':dell_all_probel(time_publc), 'link': dell_all_probel('https://www.avito.ru' + link)}
+# 				write_csv(main_info)
+# 				count +=1
+# 				print(count)
+# 			else:
+# 				continue
 	
-	# try:
-	# 	name = soup.find('a', class_='item-description-title-link').find('span').text.strip()
-	# except:
-	# 	name =''
-	# print(main_info)
+# 	# try:
+# 	# 	name = soup.find('a', class_='item-description-title-link').find('span').text.strip()
+# 	# except:
+# 	# 	name =''
+# 	# print(main_info)
 
-def make_all(url, useragent=None, proxy=None):
-	print('mk_all', url,useragent, proxy)
-	html = get_html(url, useragent, proxy)
-	get_page_date(html)
+# def make_all(url, useragent=None, proxy=None):
+# 	print('mk_all', url,useragent, proxy)
+# 	html = get_html(url, useragent, proxy)
+# 	get_page_date(html)
 
-def get_all_links_page(first_page, useragent=None, proxy=None):
-	for_last_page = get_html(first_page, useragent, proxy)
-	last_page = get_last_page(for_last_page)
-	all_pages = []
-	for i in range(1, last_page):
-		# print('Страница №', i)
-		url = template_task + str(i)
-		all_pages.append(url)
-	return all_pages
+# def get_all_links_page(first_page, useragent=None, proxy=None):
+# 	for_last_page = get_html(first_page, useragent, proxy)
+# 	last_page = get_last_page(for_last_page)
+# 	all_pages = []
+# 	for i in range(1, last_page):
+# 		# print('Страница №', i)
+# 		url = template_task + str(i)
+# 		all_pages.append(url)
+# 	return all_pages
 	
 
 
 def main():
-	start = datetime.now()
+	# start = datetime.now()
 	first_page = "https://www.avito.ru/krasnodarskiy_kray/mototsikly_i_mototehnika/mototsikly?p=1"
-	usr_agnts = open('/python_scripts/user_agents.txt').read().split('\n')
-	proxys = open('/python_scripts/proxy_list.txt').read().split('\n')
-	all_links_page = []
-	count = 0
-	while len(all_links_page) == 0:
-		sleep(uniform(3,6))
-		proxy = {'http': 'http://'+choice(proxys)}
-		usr_agnt = {'User-Agent': choice(usr_agnts)}
-		try:
-			all_links_page = get_all_links_page(first_page)
-		except:
-			continue
-		all_links_page = get_all_links_page(first_page)
-		count += 1
-		print(count)
-	for task in range(1000):
-		sleep(uniform(3,6))
-		proxy = {'http': 'http://'+choice(proxys)}
-		usr_agnt = {'User-Agent': choice(usr_agnts)}
-		print(proxy, usr_agnt)
-		for num_list in all_links_page:
-			try:
-				make_all(num_list, usr_agnt, proxy)
-			except:
-				continue			
-			make_all(num_list, usr_agnt, proxy)
-	end = datetime.now()
-	total = end - start
-	print(str(total))
+	main_page = page_info(first_page)
+	html_text = main_page.get_html()
+	last_page = main_page.get_last_page(html_text)
+	print(last_page)
+
+	# usr_agnts = open('/python_scripts/user_agents.txt').read().split('\n')
+	# proxys = open('/python_scripts/proxy_list.txt').read().split('\n')
+	# all_links_page = []
+	# count = 0
+	# while len(all_links_page) == 0:
+	# 	sleep(uniform(3,6))
+	# 	proxy = {'http': 'http://'+choice(proxys)}
+	# 	usr_agnt = {'User-Agent': choice(usr_agnts)}
+	# 	try:
+	# 		all_links_page = get_all_links_page(first_page)
+	# 	except:
+	# 		continue
+	# 	all_links_page = get_all_links_page(first_page)
+	# 	count += 1
+	# 	print(count)
+	# for task in range(1000):
+	# 	sleep(uniform(3,6))
+	# 	proxy = {'http': 'http://'+choice(proxys)}
+	# 	usr_agnt = {'User-Agent': choice(usr_agnts)}
+	# 	print(proxy, usr_agnt)
+	# 	for num_list in all_links_page:
+	# 		try:
+	# 			make_all(num_list, usr_agnt, proxy)
+	# 		except:
+	# 			continue			
+	# 		make_all(num_list, usr_agnt, proxy)
+	# end = datetime.now()
+	# total = end - start
+	# print(str(total))
 		
 
 
